bd1e9d4ed97a97bd8b3e822d68e8c3a54d3da914
Raul Campero <ruferce1@gmail.com>
2025-05-03 21:04:16 -0600

    actualización de tabla

---
+++ backend/prueba1.py
@@ -0,0 +1,19 @@
+# Import packages
+from dash import Dash, html, dash_table
+import pandas as pd
+
+# Incorporate data
+df = pd.read_csv('persona_fisica.csv')
+
+# Initialize the app
+app = Dash()
+
+# App layout
+app.layout = [
+    html.Div(children='Mi Primera App en Dash'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+]
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
