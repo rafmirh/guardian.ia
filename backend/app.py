96cc74059435a96a4592bdc8064fc8f82cb8611e
kilodemichote <maxesetalcrak@gmail.com>
2025-05-18 01:12:24 -0600

    ppp

--- backend/app.py
+++ backend/app.py
@@ -23,10 +23,6 @@ def content_page():
 def perfil_page():
     return render_template('perfil.html')
 
-@app.route('/trazabilidad')
-def content_page():
-    return render_template('trazabilidad.html')
-
 @app.route('/dashboard')
 def show_plotly_dashboard():
     dashboard = create_dashboard_from_csv("persona_fisica.csv")
@@ -35,6 +31,10 @@ def show_plotly_dashboard():
         return render_template('dashboard.html', plot_div=plotly_html)
     else:
         return "Error al generar el dashboard.", 500
+
+@app.route('/trazabilidad')
+def trazabilidad_page():
+    return render_template('trazabilidad.html')
 
 # Tabla con Dash (respetando tus códigos base)
 dash_app = Dash(__name__, server=app, url_base_pathname='/table_dash/')
