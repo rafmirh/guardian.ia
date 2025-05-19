b02d63d8ebc4217d59a02f717997552a34c20660
Rafael MSI <g.fdc@hotmail.com>
2025-05-18 22:18:46 -0600

    HTML CSS DASHBOARD

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
 from flask_cors import CORS
-import pandas as pd
-from dashboard import create_dashboard_from_csv
 
-# Dash para la tabla
-from dash import Dash, html, dash_table
-
-# Crear la app Flask
+# Create Flask app
 app = Flask(__name__)
 CORS(app)
 
-# Rutas de Flask
+# Routes
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -23,28 +18,15 @@ def content_page():
 def perfil_page():
     return render_template('perfil.html')
 
-@app.route('/dashboard')
-def show_plotly_dashboard():
-    dashboard = create_dashboard_from_csv("persona_fisica.csv")
-    if dashboard:
-        plotly_html = dashboard.render_dashboard()
-        return render_template('dashboard.html', plot_div=plotly_html)
-    else:
-        return "Error al generar el dashboard.", 500
-
 @app.route('/trazabilidad')
 def trazabilidad_page():
     return render_template('trazabilidad.html')
 
-# Tabla con Dash (respetando tus códigos base)
-dash_app = Dash(__name__, server=app, url_base_pathname='/table_dash/')
+@app.route('/dashboard')
+def show_plotly_dashboard():
+    return render_template('dashboard.html')
 
-df = pd.read_csv('persona_fisica.csv')
 
-dash_app.layout = html.Div(children=[
-    html.H1('Mi tabla en Dash'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
-])
-
+# Run app
 if __name__ == '__main__':
     app.run(debug=True)
